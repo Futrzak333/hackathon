23901bfe74fd9e18371453c2b3f9e9fe4c0cff61
varodan <jimmy02354@gmail.com>
2022-04-08 20:22:23 +0200

    Kolejna zmiana

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Pierwsza zmiana lsksks
+          oki oki
         </p>
         <a
           className="App-link"
